b44c13ba5f78860ec4868a19ef3f686b7c690ccc
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-01-20 21:02:50 +0100

    Code improvement

--- lib/reverse_audio.dart
+++ lib/reverse_audio.dart
@@ -3,14 +3,12 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ReverseAudio {
-  static const MethodChannel _channel =
-      const MethodChannel('reverse_audio');
+  static const MethodChannel _channel = const MethodChannel('reverse_audio');
 
   static Future<String> reverseFile(String originPath, String destPath) async {
-    final String result = await _channel.invokeMethod('reverseFile', {
+    return await _channel.invokeMethod('reverseFile', {
       'originPath': originPath,
       'destPath': destPath,
     });
-    return result;
   }
 }
